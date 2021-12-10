8b9af174cf6fe94a9910a360ca713e99d5eeee59
Dewey Dunnington <dewey@fishandwhistle.net>
2021-12-10 13:45:16 -0400

    nix flat representations

--- tests/testthat/test-schema.R
+++ tests/testthat/test-schema.R
@@ -14,8 +14,4 @@ test_that("extension schemas can be created", {
 
   expect_s3_class(geoarrow_schema_dense_geometrycollection(list(geoarrow_schema_point())), "carrow_schema")
   expect_s3_class(geoarrow_schema_sparse_geometrycollection(list(geoarrow_schema_point())), "carrow_schema")
-
-  expect_s3_class(geoarrow_schema_flat_linestring(), "carrow_schema")
-  expect_s3_class(geoarrow_schema_flat_polygon(), "carrow_schema")
-  expect_s3_class(geoarrow_schema_flat_multi(geoarrow_schema_point()), "carrow_schema")
 })
