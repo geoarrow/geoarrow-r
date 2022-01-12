cca10bef9f1e0e4b4245fbb4e3137a273f4d030e
Dewey Dunnington <dewey@fishandwhistle.net>
2022-01-11 20:39:35 -0400

    simplify tests for lienstring

--- tests/testthat/test-handle-geoarrow-linestring.R
+++ tests/testthat/test-handle-geoarrow-linestring.R
@@ -1,163 +1,35 @@
 
-test_that("geoarrow point reader works for linestring<xy>", {
-  features <- wk::wk_linestring(
-    wk::xy(1:10, 11:20),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(features, schema = geoarrow_schema_linestring())
+test_that("geoarrow point reader works for linestring", {
+  coords_base <- wk::xy(1:10, 11:20)
 
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = FALSE, has_m = FALSE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
-})
+  for (coord_container_format in c("+l", "+L", "+w:5")) {
+    for (coord_dim in c("xy", "xyz", "xym", "xyzm")) {
+      dims_exploded <- strsplit(coord_dim, "")[[1]]
+      features <- wk::wk_linestring(
+        wk::as_xy(coords_base, dims = dims_exploded),
+        feature_id = c(rep(1, 5), rep(2, 5))
+      )
 
-test_that("geoarrow point reader works for linestring<xyz>", {
-  features <- wk::wk_linestring(
-    wk::xyz(1:10, 11:20, 21:30),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(
-    features,
-    schema = geoarrow_schema_linestring(
-      point = geoarrow_schema_point(dim = "xyz", nullable = FALSE)
-    )
-  )
+      features_array <- geoarrow_create(
+        features,
+        schema = geoarrow_schema_linestring(
+          format = coord_container_format,
+          point = geoarrow_schema_point(dim = coord_dim, nullable = TRUE)
+        )
+      )
 
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = TRUE, has_m = FALSE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
-})
-
-test_that("geoarrow point reader works for linestring<xym>", {
-  features <- wk::wk_linestring(
-    wk::xym(1:10, 11:20, 31:40),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(
-    features,
-    schema = geoarrow_schema_linestring(
-      point = geoarrow_schema_point(dim = "xym", nullable = FALSE)
-    )
-  )
-
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = FALSE, has_m = TRUE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
-})
-
-test_that("geoarrow point reader works for linestring<xyzm>", {
-  features <- wk::wk_linestring(
-    wk::xyzm(1:10, 11:20, 21:30, 31:40),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(
-    features,
-    schema = geoarrow_schema_linestring(
-      point = geoarrow_schema_point(dim = "xyzm", nullable = FALSE)
-    )
-  )
-
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = TRUE, has_m = TRUE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
-})
-
-test_that("geoarrow point reader works for linestring<struct xy>", {
-  features <- wk::wk_linestring(
-    wk::xy(1:10, 11:20),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(
-    features,
-    schema = geoarrow_schema_linestring(
-      point = geoarrow_schema_point_struct(dim = "xy", nullable = FALSE)
-    )
-  )
-
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = FALSE, has_m = FALSE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
-})
-
-test_that("geoarrow point reader works for linestring<struct xyz>", {
-  features <- wk::wk_linestring(
-    wk::xyz(1:10, 11:20, 21:30),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(
-    features,
-    schema = geoarrow_schema_linestring(
-      point = geoarrow_schema_point_struct(dim = "xyz", nullable = FALSE)
-    )
-  )
-
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = TRUE, has_m = FALSE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
-})
-
-test_that("geoarrow point reader works for linestring<struct xym>", {
-  features <- wk::wk_linestring(
-    wk::xym(1:10, 11:20, 31:40),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(
-    features,
-    schema = geoarrow_schema_linestring(
-      point = geoarrow_schema_point_struct(dim = "xym", nullable = FALSE)
-    )
-  )
-
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = FALSE, has_m = TRUE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
-})
-
-test_that("geoarrow point reader works for linestring<struct xyzm>", {
-  features <- wk::wk_linestring(
-    wk::xyzm(1:10, 11:20, 21:30, 31:40),
-    feature_id = c(rep(1, 3), rep(2, 7))
-  )
-  features_array <- geoarrow_create(
-    features,
-    schema = geoarrow_schema_linestring(
-      point = geoarrow_schema_point_struct(dim = "xyzm", nullable = FALSE)
-    )
-  )
-
-  expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
-  expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
-  expect_identical(
-    wk::wk_vector_meta(features_array),
-    data.frame(geometry_type = 2L, size = 2, has_z = TRUE, has_m = TRUE)
-  )
-  expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
+      expect_identical(wk_handle(features_array, wk::wkb_writer()), features)
+      expect_identical(wk::as_wkt(features_array), wk::as_wkt(features))
+      expect_identical(
+        wk::wk_vector_meta(features_array),
+        data.frame(
+          geometry_type = 2L,
+          size = 2,
+          has_z = "z" %in% dims_exploded,
+          has_m = "m" %in% dims_exploded
+        )
+      )
+      expect_identical(wk::wk_meta(features_array), wk::wk_meta(features))
+    }
+  }
 })
