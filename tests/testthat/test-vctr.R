87f40c78151bfd722ecb494ab0d2c7d97e9c64c7
Dewey Dunnington <dewey@fishandwhistle.net>
2022-01-25 10:48:10 -0400

    more tests for vctr

--- tests/testthat/test-vctr.R
+++ tests/testthat/test-vctr.R
@@ -9,6 +9,16 @@ test_that("geoarrow vector class works", {
 
 test_that("geoarrow format() works for all extensions", {
   expect_identical(
+    format(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_wkt())),
+    "POINT (1 2)"
+  )
+
+  expect_identical(
+    format(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_wkb())),
+    "POINT (1 2)"
+  )
+
+  expect_identical(
     format(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")))),
     "POINT (1 2)"
   )
@@ -31,6 +41,16 @@ test_that("geoarrow format() works for all extensions", {
 
 test_that("geoarrow as.character() works for all extensions", {
   expect_identical(
+    as.character(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_wkt())),
+    "POINT (1 2)"
+  )
+
+  expect_identical(
+    as.character(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_wkb())),
+    "POINT (1 2)"
+  )
+
+  expect_identical(
     as.character(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")))),
     "POINT (1 2)"
   )
@@ -51,24 +71,58 @@ test_that("geoarrow as.character() works for all extensions", {
   )
 })
 
-test_that("geoarrow as.character() works for all extensions", {
+test_that("vctrs support works for all extensions", {
+  vctr <- as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_wkt())
+  expect_true(vctrs::vec_is(vctr))
+  expect_s3_class(
+    vctrs::vec_restore(vctrs::vec_proxy(vctr), vctr),
+    "narrow_vctr_geoarrow_wkt"
+  )
+
+  vctr <- as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_wkb())
+  expect_true(vctrs::vec_is(vctr))
+  expect_s3_class(
+    vctrs::vec_restore(vctrs::vec_proxy(vctr), vctr),
+    "narrow_vctr_geoarrow_wkb"
+  )
+
+  vctr <- as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")))
+  expect_true(vctrs::vec_is(vctr))
+  expect_s3_class(
+    vctrs::vec_restore(vctrs::vec_proxy(vctr), vctr),
+    "narrow_vctr_geoarrow_point"
+  )
+
+  vctr <- as_geoarrow_vctr(wk::wkt(c("LINESTRING (1 2, 3 4)")))
+  expect_true(vctrs::vec_is(vctr))
+  expect_s3_class(
+    vctrs::vec_restore(vctrs::vec_proxy(vctr), vctr),
+    "narrow_vctr_geoarrow_linestring"
+  )
+
+  vctr <- as_geoarrow_vctr(wk::wkt(c("POLYGON ((1 2, 3 4, 5 6, 1 2))")))
+  expect_true(vctrs::vec_is(vctr))
+  expect_s3_class(
+    vctrs::vec_restore(vctrs::vec_proxy(vctr), vctr),
+    "narrow_vctr_geoarrow_polygon"
+  )
+
+  vctr <- as_geoarrow_vctr(wk::wkt(c("MULTIPOINT ((1 2), (3 4))")))
+  expect_true(vctrs::vec_is(vctr))
+  expect_s3_class(
+    vctrs::vec_restore(vctrs::vec_proxy(vctr), vctr),
+    "narrow_vctr_geoarrow_multi"
+  )
+})
+
+test_that("format and as.character() work for geojson extension", {
   expect_identical(
-    as.character(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")))),
+    format(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_geojson())),
     "POINT (1 2)"
   )
 
   expect_identical(
-    as.character(as_geoarrow_vctr(wk::wkt(c("LINESTRING (1 2, 3 4)")))),
-    "LINESTRING (1 2, 3 4)"
-  )
-
-  expect_identical(
-    as.character(as_geoarrow_vctr(wk::wkt(c("POLYGON ((1 2, 3 4, 5 6, 1 2))")))),
-    "POLYGON ((1 2, 3 4, 5 6, 1 2))"
-  )
-
-  expect_identical(
-    as.character(as_geoarrow_vctr(wk::wkt(c("MULTIPOINT ((1 2), (3 4))")))),
-    "MULTIPOINT ((1 2), (3 4))"
+    as.character(as_geoarrow_vctr(wk::wkt(c("POINT (1 2)")), schema = geoarrow_schema_geojson())),
+    "POINT (1 2)"
   )
 })
