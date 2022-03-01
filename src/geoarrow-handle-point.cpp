aef5f57a1a9403209419862e2a7966ed807d2efd
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-01 13:24:01 -0400

    start to reorganize

--- src/geoarrow-handle-point.cpp
+++ src/geoarrow-handle-point.cpp
@@ -3,7 +3,7 @@
 #include <Rinternals.h>
 #include "wk-v1.h"
 #include "narrow.h"
-#include "internal/geoarrow.hpp"
+#include "internal/geoarrow-cpp/geoarrow.hpp"
 #include "util.h"
 
 #define CPP_START                         \
