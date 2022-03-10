8a3a26e9988eaad0bd8f03b2bcbc8cb11bdbefc6
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-10 15:28:00 -0400

    add example files

---
+++ data-raw/example_files.R
@@ -0,0 +1,130 @@
+
+library(geoarrow)
+
+# add attributes to nc
+nc_tbl <- sf::st_set_geometry(
+  sf::read_sf(system.file("shape/nc.shp", package = "sf")),
+  NULL
+)
+nc_tbl <- tibble::as_tibble(nc_tbl)
+nc_tbl$geometry <- geoarrow_example_wkt$nc
+nc_tbl_spherical <- nc_tbl
+wk::wk_is_geodesic(nc_tbl_spherical) <- TRUE
+
+
+# make tibbles for each example
+simple_example_names <- setdiff(names(geoarrow_example_wkt), "nc")
+example_tbl <- lapply(
+  geoarrow_example_wkt[simple_example_names],
+  function(ex) {
+    tibble::tibble(row_num = seq_along(ex), geometry = ex)
+  }
+)
+
+example_tbl$nc <- nc_tbl
+example_tbl$nc_spherical <- nc_tbl_spherical
+
+# write files with multiple encodings
+for (name in names(example_tbl)) {
+  # ipc
+  write_geoarrow_ipc_stream(
+    example_tbl[[name]],
+    glue::glue("inst/example_ipc_stream/{name}-default.ipc"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_default(
+      example_tbl[[name]],
+      point = geoarrow_schema_point()
+    )
+  )
+
+  write_geoarrow_ipc_stream(
+    example_tbl[[name]],
+    glue::glue("inst/example_ipc_stream/{name}-point-struct.ipc"),
+    schema = geoarrow_schema_default(
+      example_tbl[[name]],
+      point = geoarrow_schema_point_struct()
+    )
+  )
+
+  write_geoarrow_ipc_stream(
+    example_tbl[[name]],
+    glue::glue("inst/example_ipc_stream/{name}-wkt.ipc"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_wkt()
+  )
+
+  write_geoarrow_ipc_stream(
+    example_tbl[[name]],
+    glue::glue("inst/example_ipc_stream/{name}-wkb.ipc"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_wkb()
+  )
+
+  # feather
+  write_geoarrow_feather(
+    example_tbl[[name]],
+    glue::glue("inst/example_feather/{name}-default.feather"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_default(
+      example_tbl[[name]],
+      point = geoarrow_schema_point()
+    )
+  )
+
+  write_geoarrow_feather(
+    example_tbl[[name]],
+    glue::glue("inst/example_feather/{name}-point-struct.feather"),
+    schema = geoarrow_schema_default(
+      example_tbl[[name]],
+      point = geoarrow_schema_point_struct()
+    )
+  )
+
+  write_geoarrow_feather(
+    example_tbl[[name]],
+    glue::glue("inst/example_feather/{name}-wkt.feather"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_wkt()
+  )
+
+  write_geoarrow_feather(
+    example_tbl[[name]],
+    glue::glue("inst/example_feather/{name}-wkb.feather"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_wkb()
+  )
+
+  # parquet
+  write_geoarrow_parquet(
+    example_tbl[[name]],
+    glue::glue("inst/example_parquet/{name}-default.parquet"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_default(
+      example_tbl[[name]],
+      point = geoarrow_schema_point()
+    )
+  )
+
+  write_geoarrow_parquet(
+    example_tbl[[name]],
+    glue::glue("inst/example_parquet/{name}-point-struct.parquet"),
+    schema = geoarrow_schema_default(
+      example_tbl[[name]],
+      point = geoarrow_schema_point_struct()
+    )
+  )
+
+  write_geoarrow_parquet(
+    example_tbl[[name]],
+    glue::glue("inst/example_parquet/{name}-wkt.parquet"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_wkt()
+  )
+
+  write_geoarrow_parquet(
+    example_tbl[[name]],
+    glue::glue("inst/example_parquet/{name}-wkb.parquet"),
+    compression = "uncompressed",
+    schema = geoarrow_schema_wkb()
+  )
+}
