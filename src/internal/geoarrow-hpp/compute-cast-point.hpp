b0d2fcbbe97edfb25b41b5dbb493a685c21ac9ae
Dewey Dunnington <dewey@fishandwhistle.net>
2022-03-31 09:45:48 -0300

    start towards handling multiple dimensions

--- src/internal/geoarrow-hpp/compute-cast-point.hpp
+++ src/internal/geoarrow-hpp/compute-cast-point.hpp
@@ -12,44 +12,51 @@ namespace geoarrow {
 
 class PointArrayBuilder: public ComputeBuilder {
 public:
-    PointArrayBuilder() {}
+    PointArrayBuilder():
+      dimensions_(util::Dimensions::DIMENSIONS_UNKNOWN),
+      builder_xy_(2),
+      builder_xyz_(3),
+      builder_xym_(3),
+      builder_xyzm_(4),
+      builder_(&builder_xy_) {
+        builder_xy_.child().set_name("xy");
+        builder_xyz_.child().set_name("xyz");
+        builder_xym_.child().set_name("xym");
+        builder_xyzm_.child().set_name("xyzm");
+    }
 
     void reserve(int64_t additional_capacity) {
-        builder_.reserve(additional_capacity);
+        builder_->reserve(additional_capacity);
     }
 
     void shrink() {
         ArrayBuilder::shrink();
-        builder_.shrink();
+        builder_->shrink();
     }
 
     void release(struct ArrowArray* array_data, struct ArrowSchema* schema) {
         shrink();
-        builder_.release(array_data, schema);
+        builder_->release(array_data, schema);
     }
 
     const char* get_format() {
-        return builder_.get_format();
+        return builder_->get_format();
     }
 
     void new_dimensions(util::Dimensions dimensions) {
-        if (builder_.item_size() == -1) {
+        if (dimensions_ == util::Dimensions::DIMENSIONS_UNKNOWN) {
             switch (dimensions) {
             case util::Dimensions::XYZ:
-                builder_.set_item_size(3);
-                builder_.child().set_name("xyz");
+                builder_ = &builder_xyz_;
                 break;
             case util::Dimensions::XYM:
-                builder_.set_item_size(3);
-                builder_.child().set_name("xym");
+                builder_ = &builder_xym_;
                 break;
             case util::Dimensions::XYZM:
-                builder_.set_item_size(4);
-                builder_.child().set_name("xyzm");
+                builder_ = &builder_xyzm_;
                 break;
             default:
-                builder_.set_item_size(2);
-                builder_.child().set_name("xy");
+                builder_ = &builder_xy_;
                 break;
             }
         } else if (dimensions != dimensions_) {
@@ -62,15 +69,15 @@ public:
 
     Result null_feat() {
         double empty_coord[] = {NAN, NAN, NAN, NAN};
-        builder_.child().write_buffer(empty_coord, builder_.item_size());
-        builder_.finish_elements(1, false);
+        builder_->child().write_buffer(empty_coord, builder_->item_size());
+        builder_->finish_elements(1, false);
         return Result::ABORT_FEATURE;
     }
 
     Result geom_start(util::GeometryType geometry_type, int32_t size) {
         if (size == 0) {
             double empty_coord[] = {NAN, NAN, NAN, NAN};
-            coords(empty_coord, 1, builder_.item_size());
+            coords(empty_coord, 1, builder_->item_size());
         } else if (size > 1 && geometry_type == util::GeometryType::MULTIPOINT) {
             throw util::IOException("Can't write MULTIPOINT[%d] as POINT", size);
         } else if (geometry_type != util::GeometryType::POINT) {
@@ -81,14 +88,20 @@ public:
     }
 
     Result coords(const double* coord, int64_t n, int32_t coord_size) {
-        builder_.child().write_buffer(coord, n * coord_size);
-        builder_.finish_elements(n);
+        builder_->child().write_buffer(coord, n * coord_size);
+        builder_->finish_elements(n);
         return Result::CONTINUE;
     }
 
 private:
+    using Float64ListBuilder =
+        arrow::hpp::builder::FixedSizeListArrayBuilder<arrow::hpp::builder::Float64ArrayBuilder>;
     util::Dimensions dimensions_;
-    arrow::hpp::builder::FixedSizeListArrayBuilder<arrow::hpp::builder::Float64ArrayBuilder> builder_;
+    Float64ListBuilder builder_xy_;
+    Float64ListBuilder builder_xyz_;
+    Float64ListBuilder builder_xym_;
+    Float64ListBuilder builder_xyzm_;
+    Float64ListBuilder* builder_;
 };
 
 }
