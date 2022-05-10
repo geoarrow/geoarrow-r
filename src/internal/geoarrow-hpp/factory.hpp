f298c7a5cf9d6f65bb4a693e874d201320c689f7
Dewey Dunnington <dewey@fishandwhistle.net>
2022-05-09 22:14:08 -0300

    simplify factory now that there isn't a point struct

--- src/internal/geoarrow-hpp/factory.hpp
+++ src/internal/geoarrow-hpp/factory.hpp
@@ -9,202 +9,21 @@ namespace geoarrow {
 
 namespace {
 
-// autogen factory start
-ArrayView* create_view_point(struct ArrowSchema* schema, Meta& point_meta) {
-
-    switch (point_meta.storage_type_) {
-    case util::StorageType::FixedSizeList:
-        return new PointArrayView(schema);
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.point");
-    }
-
-}
-
-ArrayView* create_view_linestring(struct ArrowSchema* schema,
-                                          Meta& linestring_meta) {
-    Meta point_meta(schema->children[0]);
-
-
-    switch (linestring_meta.storage_type_) {
-    case util::StorageType::List:
-
-            switch (point_meta.storage_type_) {
-            case util::StorageType::FixedSizeList:
-                return new LinestringArrayView(schema);
-
-            default:
-                throw Meta::ValidationError(
-                    "Unsupported storage type for extension geoarrow.point");
-            }
-
-        break;
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.linestring");
-    }
-
-}
-
-ArrayView* create_view_polygon(struct ArrowSchema* schema, Meta& polygon_meta) {
-    Meta linestring_meta(schema->children[0]);
-    Meta point_meta(schema->children[0]->children[0]);
-
-
-    switch (polygon_meta.storage_type_) {
-    case util::StorageType::List:
-
-            switch (linestring_meta.storage_type_) {
-            case util::StorageType::List:
-
-                        switch (point_meta.storage_type_) {
-                        case util::StorageType::FixedSizeList:
-                            return new PolygonArrayView(schema);
-
-                        default:
-                            throw Meta::ValidationError(
-                                "Unsupported storage type for extension geoarrow.point");
-                        }
-
-                break;
-
-            default:
-                throw Meta::ValidationError(
-                    "Unsupported storage type for extension geoarrow.linestring");
-            }
-
-        break;
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.polygon");
-    }
-
-}
-
-ArrayView* create_view_multipoint(struct ArrowSchema* schema,
-                                          Meta& multi_meta, Meta& point_meta) {
-
-
-
-    switch (multi_meta.storage_type_) {
-    case util::StorageType::List:
-        switch (point_meta.storage_type_) {
-    case util::StorageType::FixedSizeList:
-        return new CollectionArrayView<PointArrayView>(schema);
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.point");
-    }
-        break;
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.geometrycollection");
-    }
-
-}
-
-ArrayView* create_view_multilinestring(struct ArrowSchema* schema,
-                                               Meta& multi_meta,
-                                               Meta& linestring_meta) {
-    Meta point_meta(schema->children[0]->children[0]);
-
-
-    switch (multi_meta.storage_type_) {
-    case util::StorageType::List:
-        switch (linestring_meta.storage_type_) {
-    case util::StorageType::List:
-
-        switch (point_meta.storage_type_) {
-        case util::StorageType::FixedSizeList:
-            return new CollectionArrayView<LinestringArrayView>(schema);
-
-        default:
-            throw Meta::ValidationError(
-                "Unsupported storage type for extension geoarrow.point");
-        }
-
-        break;
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.linestring");
-    }
-        break;
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.geometrycollection");
-    }
-
-}
-
-ArrayView* create_view_multipolygon(struct ArrowSchema* schema,
-                                            Meta& multi_meta, Meta& polygon_meta) {
-    Meta linestring_meta(schema->children[0]->children[0]);
-    Meta point_meta(schema->children[0]->children[0]->children[0]);
-
-
-    switch (multi_meta.storage_type_) {
-    case util::StorageType::List:
-        switch (polygon_meta.storage_type_) {
-    case util::StorageType::List:
-
-        switch (linestring_meta.storage_type_) {
-        case util::StorageType::List:
-
-                switch (point_meta.storage_type_) {
-                case util::StorageType::FixedSizeList:
-                    return new CollectionArrayView<PolygonArrayView>(schema);
-
-                default:
-                    throw Meta::ValidationError(
-                        "Unsupported storage type for extension geoarrow.point");
-                }
-
-            break;
-
-        default:
-            throw Meta::ValidationError(
-                "Unsupported storage type for extension geoarrow.linestring");
-        }
-
-        break;
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.polygon");
-    }
-        break;
-
-    default:
-        throw Meta::ValidationError(
-            "Unsupported storage type for extension geoarrow.geometrycollection");
-    }
-
-}
-// autogen factory end
-
-
 ArrayView* create_view_collection(struct ArrowSchema* schema, Meta& multi_meta) {
     Meta child_meta(schema->children[0]);
 
     switch (child_meta.extension_) {
     case util::Extension::Point:
-        return create_view_multipoint(schema, multi_meta, child_meta);
+        return new CollectionArrayView<PointArrayView>(schema);
 
     case util::Extension::Linestring:
-        return create_view_multilinestring(schema, multi_meta, child_meta);
+        return new CollectionArrayView<LinestringArrayView>(schema);
 
     case util::Extension::Polygon:
-        return create_view_multipolygon(schema, multi_meta, child_meta);
+        return new CollectionArrayView<PolygonArrayView>(schema);
     default:
-        throw Meta::ValidationError("Unsupported extension type for child of geoarrow.geometrycollection");
+        throw Meta::ValidationError(
+            "Unsupported extension type for child of geoarrow.geometrycollection");
     }
 }
 
@@ -246,13 +65,13 @@ ArrayView* create_view(struct ArrowSchema* schema) {
 
     switch (geoarrow_meta.extension_) {
     case util::Extension::Point:
-        return create_view_point(schema, geoarrow_meta);
+        return new PointArrayView(schema);
 
     case util::Extension::Linestring:
-        return create_view_linestring(schema, geoarrow_meta);
+        return new LinestringArrayView(schema);
 
     case util::Extension::Polygon:
-        return create_view_polygon(schema, geoarrow_meta);
+        return new PolygonArrayView(schema);
 
     case util::Extension::MultiPoint:
     case util::Extension::MultiLinestring:
